1cb3410ba0cda1b64dd53ed8427f0b3c8d800e39
Максим Давлеев <davleevkam@gmail.com>
2023-04-25 01:08:19 +0300

    Категории, жанры

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from reviews.models import Category, Genre
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+        lookup_field = 'slug'
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+        lookup_field = 'slug'
---
+++ api_yamdb/api/views.py
@@ -0,0 +1,22 @@
+from rest_framework import filters
+
+from reviews.models import Category, Genre
+
+from .mixins import CreateListDestroyViewSet
+from .serializers import CategorySerializer, GenreSerializer
+
+
+class CategoryViewSet(CreateListDestroyViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+    lookup_field = 'slug'
+
+
+class GenreViewSet(CreateListDestroyViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+    lookup_field = 'slug'
