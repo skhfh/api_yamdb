4b74c289e241bc14d028bcbe90befd2fb1143f8e
Максим Давлеев <davleevkam@gmail.com>
2023-04-26 13:46:59 +0300

    Модель Title

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,14 +1,15 @@
 from django.urls import include, path
-
 from rest_framework import routers
 
-from .views import CategoryViewSet, GenreViewSet
+from .views import CategoryViewSet, GenreViewSet, TitleViewSet
 
 app_name = 'api'
 
 router = routers.DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='categories')
 router.register(r'genres', GenreViewSet, basename='genres')
+router.register(r'titles', TitleViewSet, basename='titles')
+
 
 urlpatterns = [
     path('v1/', include(router.urls)),
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -15,3 +15,20 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.PositiveIntegerField()
+    rating = models.IntegerField()
+    description = models.TextField()
+    category = models.ForeignKey(
+        'Category',
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='titles'
+    )
+    genre = models.ManyToManyField('Genre', related_name='titles')
+
+    def __str__(self):
+        return self.name
